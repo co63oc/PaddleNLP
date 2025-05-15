069a2576fd1229af30d422dc39fbfdd71658c2dc
co63oc <co63oc@users.noreply.github.com>
2025-05-15 17:04:06 +0800

    Fix

--- paddlenlp/transformers/auto/image_processing.py
+++ paddlenlp/transformers/auto/image_processing.py
@@ -33,6 +33,7 @@ IMAGE_PROCESSOR_MAPPING_NAMES = OrderedDict(
         ("CLIPImageProcessor", "clip"),
         ("ErnieViLImageProcessor", "ernie_vil"),
         ("ViTImageProcessor", "clipseg"),
+        ("DeiTImageProcessor", "deit"),
     ]
 )
 
@@ -40,7 +41,10 @@ IMAGE_PROCESSOR_MAPPING_NAMES = OrderedDict(
 def get_configurations():
     MAPPING_NAMES = OrderedDict()
     for key, class_name in IMAGE_PROCESSOR_MAPPING_NAMES.items():
-        import_class = importlib.import_module(f"paddlenlp.transformers.{class_name}.image_processing")
+        try:
+            import_class = importlib.import_module(f"paddlenlp.transformers.{class_name}.image_processing")
+        except:
+            import_class = importlib.import_module(f"ppdiffusers.transformers.{class_name}.image_processing")
         processor_name = getattr(import_class, key)
         name = tuple(processor_name.pretrained_init_configuration.keys())
         if MAPPING_NAMES.get(name, None) is None:
@@ -83,6 +87,8 @@ class AutoImageProcessor:
             try:
                 class_name = cls._name_mapping[init_class]
                 import_class = import_module(f"paddlenlp.transformers.{class_name}.image_processing")
+                if import_class is None:
+                    import_class = import_module(f"ppdiffusers.transformers.{class_name}.image_processing")
                 processor_class = getattr(import_class, init_class)
                 return processor_class
             except Exception:
